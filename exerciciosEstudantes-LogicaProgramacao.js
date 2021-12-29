b3251a5febdb354dec3a6f571e765b7c2f19f7d4
Joshua <joshuaamaral05@gmail.com>
2021-12-29 18:31:44 -0300

    Exercicios Estudantes- Lógica de Programação

---
+++ exerciciosEstudantes-LogicaProgramacao.js
@@ -0,0 +1,32 @@
+let listaEstudantes = ["Helena", "Arthur", "Joshua", "Nicole","Tiago"]
+let quantidadeEstudantes =listaEstudantes.length
+console.log (quantidadeEstudantes)
+
+// Utilizando o "For"
+// for (let indice = 0; indice < quantidadeEstudantes; indice++) {
+//     const alunoCorrente = listaEstudantes[indice]
+//     console.log (alunoCorrente)
+// }
+//-------------------------------------------------------------------------------------------------
+//Utilizando o "Do"
+// let listaDeEstudantes = ["Obian", "Chico", "Mário"];
+// let quantidadeDeEstudantes = listaDeEstudantes.length;
+// let inicio = 0;
+
+// do {
+// 	console.log(listaDeEstudantes[inicio]);
+// 	inicio++;
+// } while (inicio < quantidadeDeEstudantes);
+
+//-------------------------------------------------------------------------------------------------
+
+
+// Utilizando o "While"
+// let listaDosEstudantes = ["Pedro", "Carla", "Patrick"];
+// let quantidadeDosEstudantes = listaDosEstudantes.length;
+// let inicio = 0;
+
+// while (inicio < quantidadeDosEstudantes) {
+// 	console.log(listaDosEstudantes[inicio]);
+// 	inicio++;
+// }
